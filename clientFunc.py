05441db45090cf778096540cf6aaec1760865447
JohnsonLu3 <JohnsonLu94@gmail.com>
2016-12-06 14:21:07 -0500

    + Added login functionality 	creates a usr file if the ID is new 	reads from a usr file if the ID is old
    + Added new folder usr to store usr files

--- clientFunc.py
+++ clientFunc.py
@@ -1,5 +1,39 @@
+import os
+from pathlib import Path
+
 # Declare CONSTANT vars
 DEFAULT_N = 5
+USR_PATH = 'usrs/'
+EXTENDSION = '.txt'
+
+# Group Map
+# Just a quick write up of a group map for all the discussion
+# groups that will be use. Can be cleaned up some.
+# This will mainly be used to make the user file.
+# Key : group name  ,   Value : subscribed boolean(0/1)
+groups = {
+    'comp.programming': 0,
+    'comp.lang.c': 0,
+    'comp.lang.python': 0,
+    'comp.lang.javascript': 0,
+    'comp.lang.c++': 0,
+    'comp.lang.java': 0,
+
+    'math.crypto': 0,
+    'math.algro': 0,
+    'math.calc': 0,
+    'math.stats': 0,
+
+    'sb.cse114': 0,
+    'sb.cse214': 0,
+    'sb.cse219': 0,
+    'sb.cse220': 0,
+    'sb.cse300': 0,
+    'sb.cse303': 0,
+    'sb.cse310': 0,
+    'sb.cse312': 0,
+    'sb.cse320': 0
+}
 
 # help
 # Prints a list of supported commands and sub-commands.
@@ -22,6 +56,16 @@ def printHelp():
 def login(ID):
     # Do login stuff
     loggedIn = False;
+    file = Path(USR_PATH + ID +EXTENDSION)
+    if file.is_file():
+        # read file and import data
+        print("FILE IS HERE")
+        fillHisto(ID)
+    else:
+        # else create the file
+        print("FILE DOESNT EXIT")
+        createHisto(ID)
+
     return loggedIn
 
 # ag
@@ -53,3 +97,35 @@ def logout(socket):
     print("Logging out...")
     socket.close()
     exit()
+
+# createHisto
+# If a user logs inand no file exist for the ID then create a new file for that user with the
+# ID as the file name
+def createHisto(ID):
+    fileName = USR_PATH + str(ID) + EXTENDSION
+    file = open(fileName, 'w')
+
+    for i in groups.keys():
+        file.write(i)                                   # writes group name
+        file.write(",")                                 # writes sep
+        file.write(str(groups.get(i)))                  # writes sub bool for group
+        file.write("\n")                                # writes new line
+
+    file.close()
+    return
+
+# fillHisto
+#
+#
+def fillHisto(ID):
+    fileName = USR_PATH + str(ID) + EXTENDSION
+    file = open(fileName, 'r')
+
+    while 1:
+        line = file.readline()                          # read file line by line
+        # end of file has been reached
+        if line == "":
+            break
+        data = line.split(',')                          # breaks the line into its part by ','
+        if data[0] in groups.keys():                    # checks if the data is correct
+            groups[data[0]] = data[1]                   # sets the value
